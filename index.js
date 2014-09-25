fe9145ee55ead382bbce82bfaf41d4e65afe7d47
David Konsumer <konsumer@jetboystudio.com>
2014-09-24 20:06:47 -0700

    inital commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require('body-parser'),
+    errorHandler = require('errorhandler'),
+    methodOverride = require('method-override'),
+    port = parseInt(process.env.PORT, 10) || 4567;
+
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler({
+  dumpExceptions: true,
+  showStack: true
+}));
+
+console.log("Simple static server listening at http://localhost:" + port);
+app.listen(port);
